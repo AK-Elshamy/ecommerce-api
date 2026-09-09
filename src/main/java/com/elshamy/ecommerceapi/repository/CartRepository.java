048eced50dcb87844fbc49df1bc825242d87f946
Ahmed <ahmed.khalid.elshamy37@gmail.com>
2026-09-09 16:27:47 +0300

    Add cart repository user lookup

--- src/main/java/com/elshamy/ecommerceapi/repository/CartRepository.java
+++ src/main/java/com/elshamy/ecommerceapi/repository/CartRepository.java
@@ -1,7 +1,11 @@
 package com.elshamy.ecommerceapi.repository;
 
 import com.elshamy.ecommerceapi.entity.Cart;
+import com.elshamy.ecommerceapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUser(User user);
 }
