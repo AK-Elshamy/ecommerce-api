bad04f9dbcb1c9d0f5b6863d256e455c0e53ae6d
Ahmed <ahmed.khalid.elshamy37@gmail.com>
2026-09-09 16:27:44 +0300

    Configure Spring Security for JWT authentication

--- src/main/java/com/elshamy/ecommerceapi/config/SecurityConfig.java
+++ src/main/java/com/elshamy/ecommerceapi/config/SecurityConfig.java
@@ -1,30 +1,59 @@
 package com.elshamy.ecommerceapi.config;
 
+import com.elshamy.ecommerceapi.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/auth/**").permitAll()
-                                .anyRequest().authenticated()
-                );
-        return http.build();
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http) throws Exception {
 
+        http
+                // JWT → Stateless, so we don't need CSRF protection
+                .csrf(csrf -> csrf.disable())
+
+                // Don't create or use HTTP sessions for authentication
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+
+                // Authorization rules
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+
+                // Run our JWT filter before Spring's authentication filter
+                .addFilterBefore(
+                        jwtAuthFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
+
+        return http.build();
     }
 }
