7f449cebcf58a0c673bbd36005889b80f19f17d2
Ahmed <ahmed.khalid.elshamy37@gmail.com>
2026-09-10 15:00:46 +0300

    Add order history repository query

--- src/main/java/com/elshamy/ecommerceapi/repository/OrderRepository.java
+++ src/main/java/com/elshamy/ecommerceapi/repository/OrderRepository.java
@@ -4,7 +4,8 @@ import com.elshamy.ecommerceapi.entity.Order;
 import com.elshamy.ecommerceapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-}
+    List<Order> findByUserOrderByOrderDateDesc(User user);}
